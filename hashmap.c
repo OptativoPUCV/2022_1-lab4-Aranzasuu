38f95128696c176314b9e5bcd869f651cf03781a
Aranzasuu <>
2022-04-08 15:45:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,10 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-    
+    long posicion = map -> current;
+    map -> buckets[posicion] -> key = key;
+    map -> buckets[posicion] -> value = value;
+    (map -> size)++;
 
 }
 
